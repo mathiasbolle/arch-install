2ac313e55d9d3cff96dcf919ad050f4693633fa1
Mathias Bolle <mbolle2310@protonmail.com>
2022-09-16 21:22:35 +0200

    feat: basic loop

--- install_arch.sh
+++ install_arch.sh
@@ -2,21 +2,36 @@
 
 echo "welcome to the install_arch.sh script created by Mathias"
 
+# basic setup 
 loadkeys azerty # load azerty keys to keyboard
 timedatectl set-ntp true 
 lsblk #display all block devices
+
+
+# configuration drive (partitions)
 echo -n "Enter drive:"
 read drive
 echo $drive
 
-echo "Partitions from drive $drive are:"
-lsblk $drive
+changes=""
 
+while [$changes != "y"]
+do
+	echo "Partitions from drive $drive are:"
+	lsblk $drive
+
+
+	echo "are the partitions OK for drive $drive (y/n): "
+	read changes
+done
+
+echo "select the root partition, efi partition and swap partition"
 echo -n "root partition:"
 read root
 echo -n "efi partition:"
 read efi
-
+echo -n "swap partition:"
+read swap
 
 #fdisk $drive << FDISK_CMD
 
