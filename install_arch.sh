9f28b845a569e3ba7bbbc141b244016ed450d3da
Mathias Bolle <mbolle2310@protonmail.com>
2022-09-17 22:40:06 +0200

    installation arch-chroot

--- install_arch.sh
+++ install_arch.sh
@@ -35,6 +35,46 @@ read efi
 echo -n "swap partition:"
 read swap
 
-#fdisk $drive << FDISK_CMD
+# generate filesystem
+mkfs.btrfs -L root /dev/$root # create btrfs filesystem with label 'root'
+mkswap -L swap /dev/$swap # create swap with label 'swap'
+# no efi for windows?
 
-#FDISK_CMD
+# btrfs filesystem
+mount /dev/$root /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+
+umount /mnt
+mount subvol=@ /dev/$root /mnt
+mkdir -p /mnt/home
+mount subvol=@home /dev/root /mnt/home
+
+mount --mkdir /dev/$efi /mnt/boot
+swapon /dev/$swap
+
+# installation of basic arch linux software
+pacstrap /mnt base linux linux-firmware base-devel man-db neovim networkmanager
+
+genfstab -L /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/Europe/Brussels /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+
+touch /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+touch /etc/vconsole.conf
+echo "KEYMAP=azerty" >> /etc/vconsole.conf
+
+touch /etc/hostname
+echo "thinkbook" >> /etc/hostname
+
+mkinitcpio -P
+
+passwd
+
+# boot loader?
