90922cbe6e183503237aae9af91725c5b4f038af
Mathias Bolle <mbolle2310@protonmail.com>
2022-09-25 20:25:11 +0200

    fix: install paru

--- post_install.sh
+++ post_install.sh
@@ -7,8 +7,8 @@ useradd -m -G wheel mathias
 
 passwd mathias # password of user mathias
 
+# installation paru
+sudo pacman -Syu paru
 
-
-pacman -Syu rofi river zip xorg-xev wpaperd wlr-randr wget vscodium virt-manager unzip ttf-twemoji texlive-latexextra texlab teams-for-linux swaylock slurp rust-analyzer rtorrent textlive-core pacmixer nvm nmap network-manager-applet nerd-fonts-anonymous-pro neofetch libreoffice-still kotlin-language-server kotlin joshuto imv httpie grim firefox-developer-edition docker discord android-studio alacritty
-
+paru -Syu rofi river zip xorg-xev wpaperd wlr-randr wget vscodium virt-manager unzip ttf-twemoji texlive-latexextra texlab teams-for-linux swaylock slurp rust-analyzer rtorrent textlive-core pacmixer nvm nmap network-manager-applet nerd-fonts-anonymous-pro neofetch libreoffice-still kotlin-language-server kotlin joshuto imv httpie grim firefox-developer-edition docker discord android-studio alacritty
 
