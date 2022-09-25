7b1d15726de5a38222a3465f546b057f5a6e7765
Mathias Bolle <mbolle2310@protonmail.com>
2022-09-25 20:36:43 +0200

    fix: add paru instructions

--- post_install.sh
+++ post_install.sh
@@ -8,7 +8,10 @@ useradd -m -G wheel mathias
 passwd mathias # password of user mathias
 
 # installation paru
-sudo pacman -Syu paru
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
 
 paru -Syu rofi river zip xorg-xev wpaperd wlr-randr wget vscodium virt-manager unzip ttf-twemoji texlive-latexextra texlab teams-for-linux swaylock slurp rust-analyzer rtorrent textlive-core pacmixer nvm nmap network-manager-applet nerd-fonts-anonymous-pro neofetch libreoffice-still kotlin-language-server kotlin joshuto imv httpie grim firefox-developer-edition docker discord android-studio alacritty
 
