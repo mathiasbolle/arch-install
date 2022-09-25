4aadb39e15bf14fb53638391dd6a14fd8639cf05
Mathias Bolle <mbolle2310@protonmail.com>
2022-09-25 20:14:27 +0200

    feat: minimal post installation

---
+++ post_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "welcome to the post_install.sh script created by Mathias"
+
+# make user
+useradd -m -G wheel mathias
+
+passwd mathias # password of user mathias
+
+
+
+pacman -Syu rofi river zip xorg-xev wpaperd wlr-randr wget vscodium virt-manager unzip ttf-twemoji texlive-latexextra texlab teams-for-linux swaylock slurp rust-analyzer rtorrent textlive-core pacmixer nvm nmap network-manager-applet nerd-fonts-anonymous-pro neofetch libreoffice-still kotlin-language-server kotlin joshuto imv httpie grim firefox-developer-edition docker discord android-studio alacritty
+
+
